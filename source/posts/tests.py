693f1bbb82c6df3f0c389891339b409de97d7064
SGGM <gleb_somov@mail.ru>
2023-11-27 13:55:09 +0300

    blog test

--- source/posts/tests.py
+++ source/posts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user_1 = User.objects.create_user(
+            username="test_user_1",
+            password="abc1"
+        )
+        test_user_1.save()
+
+        test_post = Post.objects.create(
+            author=test_user_1,
+            title="Blog title",
+            body="Body content"
+        )
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f"{post.author}"
+        title = f"{post.title}"
+        body = f"{post.body}"
+        
+        self.assertEqual(author, "test_user_1")
+        self.assertEqual(title, "Blog title")
+        self.assertEqual(body, "Body content")
